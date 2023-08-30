ad978da79c819bef210407f80a932cef180c2522
FromWau <tobias.frommhund@aon.at>
2023-08-31 01:03:13 +0200

    WIP

--- hypr-wal.sh
+++ hypr-wal.sh
@@ -2,10 +2,14 @@
 
 changeWallpaper() {
 	# Get the list of monitors
-	monitors=("$(hyprctl monitors -j | jq -r '.[].name')")
+	monitors=(
+        "$(hyprctl monitors -j | jq -r '.[].name')"
+    )
 
 	# Get a list of random pictures from the specified directory
-	random_pictures=($(fd ".png|.jpg|.jpeg" ~/Pictures/wallpapers/ | shuf -n ${#monitors[@]}))
+	random_pictures=(
+        $(fd ".png|.jpg|.jpeg" ~/Pictures/wallpapers/ | shuf -n ${#monitors[@]})
+    )
 
 	# Loop through each monitor and set a random wallpaper
 	for i in "${!monitors[@]}"; do
@@ -23,7 +27,7 @@ changeWallpaper() {
 }
 
 updateCava() {
-    cp -r "$HOME"/.cache/wal/colors-cava.cava "$HOME"/.config/cava/config
+    cp -r "$HOME"/.cache/wal/colors-cava.cava "$HOME"/.config/cava/config &&
     cat <<EOF >> "$HOME"/.config/cava/config
 
 
@@ -36,10 +40,25 @@ EOF
     [[ $(pidof cava) != "" ]] && pkill -USR1 cava
 }
 
+updateKitty() {
+    cp -r "$HOME"/.cache/wal/colors-kitty.conf "$HOME"/.config/kitty/wal-theme.conf &&
+    [[ $(rg "^include wal-theme.conf\$" "$HOME"/.config/kitty/kitty.conf) == "" ]] &&
+        echo "Pls run following line to include wal-theme to your $HOME/.cache/wal/colors-kitty.conf:" &&
+        echo "echo 'include wal-theme.conf' >> $HOME/.cache/wal/colors-kitty.conf"
+}
+
+updateBat() {
+    [[ ! -e "$(bat --config-dir)"/themes ]] && mkdir -p "$HOME"/.config/bat/themes
+
+
+}
+
 changeWallpaper
 
 pic=$(cat "$(fd . "$HOME"/.cache/swww/ | head -1)")
 
-wal -i "$pic" --cols16 -n -q
+wal -i "$pic" --cols16 -n -q 2> /dev/null
 
 updateCava
+updateKitty
+updateBat
