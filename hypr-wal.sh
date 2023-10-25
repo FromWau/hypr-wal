fcd83a69169fec8509f078d0cb8c5ac9212fe119
tfrommhund <t.frommhund@get.systems>
2023-10-25 15:20:18 +0200

    increase saturation to 1 (maybe too much)

--- hypr-wal.sh
+++ hypr-wal.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
 changeWallpaper() {
-	# Get the list of monitors
 	monitors=(
 		$(hyprctl monitors -j | jq -r '.[].name')
 	)
 
-	# Get a list of random pictures from the specified directory
 	random_pictures=(
 		$(fd ".png|.jpg|.jpeg" ~/Pictures/wallpapers/ | shuf -n ${#monitors[@]})
 	)
 
-	# Loop through each monitor and set a random wallpaper
 	for i in "${!monitors[@]}"; do
-		# Get the current monitor and random picture
 		monitor=${monitors[$i]}
 		random_picture=${random_pictures[$i]}
 
-		# Set the wallpaper for the current monitor
 		swww img -o "$monitor" "$random_picture" \
 			--transition-step 255 \
 			--transition-fps 60 \
@@ -81,7 +76,7 @@ changeWallpaper
 
 pic=$(cat "$(fd . "$HOME"/.cache/swww/ | head -1)")
 
-wal -i "$pic" --cols16 -n -q 2>/dev/null
+wal --saturate 1.0 -i "$pic" --cols16 -n -q 2>/dev/null
 
 updateCava
 updateKitty
